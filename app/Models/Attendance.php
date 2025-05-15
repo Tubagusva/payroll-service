4a3698c2773c674ebbd2a78be57d86d3fb63e301
TEBE <124652119+Tubagusva@users.noreply.github.com>
2025-05-15 14:12:15 +0700

    Add files via upload

---
+++ app/Models/Attendance.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+class Attendance extends Model
+{
+    protected $guarded = ['id'];
+    protected $with = ['user'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function isLate()
+    {
+        $scheduleStartTime = Carbon::parse($this->schedule_start_time);
+        $startTime = Carbon::parse($this->start_time);
+
+        return $startTime->greaterThan($scheduleStartTime);
+    }
+
+    public function workDuration()
+    {
+        $startTime = Carbon::parse($this->start_time);
+        $end_time = Carbon::parse($this->end_time);
+
+        $duration = $startTime->diff($end_time);
+
+        $hours = $duration->h;
+        $minutes = $duration->i;
+
+        return $hours . ' jam ' . $minutes . ' menit';
+    }
+}
